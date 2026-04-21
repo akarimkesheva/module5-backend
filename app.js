77d27a538f4f47fcdd957d6db151e68bca211d3d
akarimkesheva <akarimkesheva@ivytech.edu>
2026-04-21 08:22:59 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express=require ("express")
+const path = require("path");
+const cors = require("cors");
 const app=express()
+const router=express.Router()
 
-app.listen(3000,function(){
-    console.log("Listening on port 3000")
-})
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(cors());
+
+
+router.get("/songs", function(req, res) {
+    const songs = [ 
+        { 
+            title: "We found love",
+            artist: "Rihanna",
+            popularity: 10,
+            releaseDate: new Date(2011, 9, 22),
+            genre: ["funk", "boogie"]
+        }, 
+        {
+            title: "Happy",
+            artist: "Pharrell Williams",
+            popularity: 10,
+            releaseDate: new Date(2013, 11, 21),
+            genre: ["funk", "boogie"]
+        }
+    ]; 
+    res.json(songs);
+});
+app.use("/api", router);
+app.listen(3000)
