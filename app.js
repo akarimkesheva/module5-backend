c0cd076b27cd8c9707a9b6c01d56bb303e55d98f
akarimkesheva <akarimkesheva@ivytech.edu>
2026-04-22 17:06:00 -0400

    Initial commit for backend

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
 const express=require ("express")
 var cors = require("cors");
 const path = require("path");
+
+const bodyParser=require('body-parser')
+const Song=require("./models/songs")
 const app=express()
 app.use(cors());
+
+app.use(bodyParser.json())
 const router=express.Router()
 
 
-router.get("/songs", function(req, res) {
-    const songs = [ 
-        { 
-            title: "We found love",
-            artist: "Rihanna",
-            popularity: 10,
-            releaseDate: new Date(2011, 9, 22),
-            genre: ["funk", "boogie"]
-        }, 
-        {
-            title: "Happy",
-            artist: "Pharrell Williams",
-            popularity: 10,
-            releaseDate: new Date(2013, 11, 21),
-            genre: ["funk", "boogie"]
-        }
-    ]; 
-    res.json(songs);
-});
+router.get("/songs", async(req, res)=> {
+    try{
+        const songs=await Song.find({})
+        res.send(songs)
+        console.log(songs)
+    }
+    catch (err){
+        console.log(err)
+    }
+    })
+
+router.get("/songs/:id", async (req,res) =>{
+    try{
+       const song= await Song. findById(req.params.id)
+       res.json(song)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+router.post("/songs",async(req, res)=>{
+    try{
+        const song=await new Song(req.body)
+        await song.save()
+        res.status(201).json(song)
+        console.log(song)
+        res.sendStatus(204)
+    }
+    catch (err){
+        res.status(400).send(err)
+    }
+       
+})
+router.put("/songs/:id", async (req, res) => { 
+    try {
+        const song = req.body;
+       
+        const updatedSong = await Song.updateOne({ _id: req.params.id }, song);
+        console.log(song);
+        res.status(200).send(updatedSong); 
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
 app.use("/api", router);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
